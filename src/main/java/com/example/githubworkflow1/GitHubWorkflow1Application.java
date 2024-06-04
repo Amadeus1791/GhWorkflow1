3f20e629ef151ef1d31e8ebc6a155e078aa2eaae
Wolfgang <myemail@gmx.at>
2024-06-04 17:14:51 +0200

    Add Springboot

---
+++ src/main/java/com/example/githubworkflow1/GitHubWorkflow1Application.java
@@ -0,0 +1,13 @@
+package com.example.githubworkflow1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubWorkflow1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubWorkflow1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubworkflow1/GitHubWorkflow1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubworkflow1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubWorkflow1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
